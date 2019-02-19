307b3d899e1137ae777ab9b373df4e39366e089d
Zsolt Galamb <gonbazsolt@gmail.com>
2019-02-19 17:15:43 +0100

    dev environment init

---
+++ Bookstore/Backend/server.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const PORT = 8080;
+
+app.listen(PORT, () => {
+  console.log(`The server is up and running port ${PORT}`);
+});
