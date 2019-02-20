3af2970b3eb6e188de0221958beb67fa41680633
Zsolt Galamb <gonbazsolt@gmail.com>
2019-02-20 05:48:23 +0100

    first/sample form creted

--- Bookstore/src/app/app.module.ts
+++ Bookstore/src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { BooksCrudComponent } from './books-crud/books-crud.component';
 import { BookStockManagerComponent } from './book-stock-manager/book-stock-manager.component';
-
-import { AppRoutingModule } from './app-routing.module';
 import { OpenPageComponent } from './open-page/open-page.component';
+
 import { BookCreateComponent } from './books-crud/book-create/book-create.component';
 import { BookListComponent } from './books-crud/book-list/book-list.component';
 import { BookModifyComponent } from './books-crud/book-modify/book-modify.component';
 import { BookDeleteComponent } from './books-crud/book-delete/book-delete.component';
+
 import { TransactionReceiveComponent } from './book-stock-manager/transaction-receive/transaction-receive.component';
 import { TransactionSellComponent } from './book-stock-manager/transaction-sell/transaction-sell.component';
 import { TransactionListComponent } from './book-stock-manager/transaction-list/transaction-list.component';
@@ -31,7 +34,8 @@ import { TransactionListComponent } from './book-stock-manager/transaction-list/
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Bookstore/src/app/books-crud/book-create/book-create.component.ts
+++ Bookstore/src/app/books-crud/book-create/book-create.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { stringify } from '@angular/core/src/render3/util';
 
 @Component({
   selector: 'app-book-create',
@@ -6,10 +8,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./book-create.component.css']
 })
 export class BookCreateComponent implements OnInit {
+  createBookForm: FormGroup;
+  categories: string[] = ['Novella', 'Regény', 'Dráma', 'Verseskötet', 'Életrajz', 'Képregény'];
+  formStatus: string;
+  isSubmitPushed: boolean = false;
 
   constructor() { }
 
   ngOnInit() {
+    this.createBookForm = new FormGroup({
+      'author': new FormControl(null, Validators.required),
+      'title': new FormControl(null, Validators.required),
+      'category': new FormControl(null, Validators.required),
+      'published': new FormControl(null, Validators.required),
+      'description': new FormControl()
+    });
+
+    this.createBookForm.setValue({
+      'author': null,
+      'title': null,
+      'category': null,
+      'published': null,
+      'description': null
+    });
+
+    this.createBookForm.statusChanges.subscribe(
+      (status) => this.formStatus = status);
   }
 
+  onSubmit() {
+    this.isSubmitPushed = true;
+
+    if (this.formStatus === "VALID") {
+      this.createBookForm.reset();
+      this.isSubmitPushed = false;
+    }
+  }
 }
